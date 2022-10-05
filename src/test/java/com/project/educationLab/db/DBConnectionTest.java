3ff34ecad87b389e0bc2eab042fffa7e89b0ed28
KIM-KYOUNG-OH <kdkg0434@gmail.com>
2022-10-05 16:36:02 +0900

    DB Connection

---
+++ src/test/java/com/project/educationLab/db/DBConnectionTest.java
@@ -0,0 +1,49 @@
+package com.project.educationLab.db;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class DBConnectionTest {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DBConnectionTest.class);
+
+    @Value("${spring.datasource.url}")
+    private String URL;
+
+    @Value("${spring.datasource.username}")
+    private String USERNAME;
+
+    @Value("${spring.datasource.password}")
+    private String PASSWORD;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String DRIVER;
+
+    @Test
+    public void propertiesTest() {
+        assertNotNull(URL);
+        assertNotNull(USERNAME);
+        assertNotNull(PASSWORD);
+        assertNotNull(DRIVER);
+    }
+
+    @Test
+    public void connectionTest() throws ClassNotFoundException, SQLException {
+        Class.forName(DRIVER);
+
+        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        assertNotNull(connection);
+    }
+}
